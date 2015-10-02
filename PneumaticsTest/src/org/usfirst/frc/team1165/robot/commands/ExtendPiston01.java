b760663727c1f10df221f729268cea5685717c5f
jkoehring <jkoehring@exponent.com>
2015-10-02 15:21:05 -0700

    Project for experimenting with pneumatics.

---
+++ PneumaticsTest/src/org/usfirst/frc/team1165/robot/commands/ExtendPiston01.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team1165.robot.commands;
+
+public class ExtendPiston01 extends PistonCommand
+{
+	public ExtendPiston01()
+	{
+		super(piston01, extend);
+	}
+}
---
+++ PneumaticsTest/src/org/usfirst/frc/team1165/robot/commands/RetractPiston01.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team1165.robot.commands;
+
+public class RetractPiston01 extends PistonCommand
+{
+	public RetractPiston01()
+	{
+		super(piston01, retract);
+	}
+}
